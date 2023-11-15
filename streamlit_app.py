fa7a2f19f90000212d8662d4f5ec0d84c6bf26ae
Francisco Badilla <36639028+fbadilla@users.noreply.github.com>
2023-11-15 03:42:49 +0000

    cambios_dos

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
-st.sidebar.header('Dashboard `version 2`')
+st.sidebar.header('DataSolutionsLABS `Almacen_Cheito`')
 
 st.sidebar.subheader('Heat map parameter')
 time_hist_color = st.sidebar.selectbox('Color by', ('temp_min', 'temp_max')) 
@@ -21,16 +21,16 @@ plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
 
 st.sidebar.markdown('''
 ---
-Created with ❤️ by [Data Professor](https://youtube.com/dataprofessor/).
+Created with ❤️ by [DataSolution Lab](https://youtube.com/).
 ''')
 
 
 # Row A
-st.markdown('### Metrics')
+st.markdown('### Metricas')
 col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+col1.metric("Ventas acumuladas mes", "5.500.000", "45%")
+col2.metric("Ventas diarias", "500.000", "-18%")
+col3.metric("Producto mas vendido", "Palta", "4%")
 
 # Row B
 seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
@@ -38,7 +38,7 @@ stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/maste
 
 c1, c2 = st.columns((7,3))
 with c1:
-    st.markdown('### Heatmap')
+    st.markdown('### Mapa de calor')
     plost.time_hist(
     data=seattle_weather,
     date='date',
@@ -50,7 +50,7 @@ with c1:
     height=345,
     use_container_width=True)
 with c2:
-    st.markdown('### Donut chart')
+    st.markdown('### Crecimiento en RRSS')
     plost.donut_chart(
         data=stocks,
         theta=donut_theta,
